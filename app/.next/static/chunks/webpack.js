336ff5d6fb21caf284a8324db2eda60274842936
itzemorai87@gmail.com <itzemorai87@gmail.com>
2022-10-11 21:37:54 +0100

    added next to git ignore

--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "394e50592ca1face"; }
+/******/ 		__webpack_require__.h = function() { return "a2bd71b1ce826518"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ app/layouts/app.layout.tsx
@@ -0,0 +1,20 @@
+import HeaderComp from "components/shared/HeaderComp";
+import SideNav from "components/shared/SideNav";
+import React, { ReactElement } from "react";
+
+const AppLayout: React.FC<{ children: ReactElement }> = ({ children }) => {
+	return (
+		<>
+			<div className="app">
+				<HeaderComp />
+				<div className="app-wrapper">
+					<SideNav />
+					<main className="app-children bg-info">{children}</main>
+				</div>
+				<p className="bg-success">footerComp</p>
+			</div>
+		</>
+	);
+};
+
+export default AppLayout;
